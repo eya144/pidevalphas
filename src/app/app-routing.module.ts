b4f416eac52396ba56e0ee1bcbe4bff1c7f6ee0e
Eya Maamouri <92765964+eya144@users.noreply.github.com>
2025-02-13 18:07:51 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RHComponent } from './rh/rh.component';
+import { LogistiqueComponent } from './logistique/logistique.component';
+import { InspectionComponent } from './inspection/inspection.component';
+import { FinanceComponent } from './finance/finance.component';
+import { ContactComponent } from './contact/contact.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ServiceComponent } from './service/service.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'produit', component: ProductComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'RH', component: RHComponent },
+  { path: 'logistique', component: LogistiqueComponent },
+  { path: 'inspection', component: InspectionComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'finance', component: FinanceComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'inscrire', component: InscriptionComponent },
+  { path: 'service', component: ServiceComponent },
+  { path: 'service', component: ServiceComponent }, 
+   { path: 'dashboard', component: DashboardComponent },
+  
+  
+
+  { path: '', redirectTo:'/home',pathMatch:'full' }
+];
+
+ 
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { LogistiqueComponent } from './logistique/logistique.component';
+import { RHComponent } from './rh/rh.component';
+import { FinanceComponent } from './finance/finance.component';
+import { InspectionComponent } from './inspection/inspection.component';
+
+import { ContactComponent } from './contact/contact.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ServiceComponent } from './service/service.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BackMenuAdminComponent } from './back-menu-admin/back-menu-admin.component';
+import { BackHeaderComponent } from './back-header/back-header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    AboutComponent,
+    LoginComponent,
+    LogistiqueComponent,
+    RHComponent,
+    FinanceComponent,
+    InspectionComponent,
+ 
+    ContactComponent,
+      InscriptionComponent,
+      ServiceComponent,
+      DashboardComponent,
+      BackMenuAdminComponent,
+      BackHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
