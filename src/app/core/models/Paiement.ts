1f94a8bb1ca5d0a3b2b66f7f302bafc33542563b
Zayneb Akremi <akremizayneb99@gmail.com>
2025-02-23 17:06:39 +0100

    CRUD + Controle de Saisie

--- src/app/core/models/Paiement.ts
+++ src/app/core/models/Paiement.ts
@@ -3,6 +3,7 @@ export interface Paiement {
     idPaiement?: number;  // Long idPaiement;
     idUtilisateur?: number;  // Long idUtilisateur;
     idContrat?: number;  // Long idContrat;
+    idFacture?: number;
     montant: number;  // float montant;
     datePaiement: string;  // Date datePaiement; (as string or Date object, depending on usage)
     payment: 'CARTE' | 'ESPECES' | 'CHÈQUE' | 'VIREMENT'; // methodePaiement (considered as a string type, but you could use an enum if needed)
